c87ea19a3cedbf20faa3a8a39da8b29efb8a78cb
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-13 11:22:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort {|x,y| x <=> y}
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+# array.sort {|x,y| -(x <=> y)} ???
+end
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+# array.sort_by(&:length)
+# array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+
+# array.swap!(array[1],array[2])
+# array.sort_by{|x,y| x[1], y[2] = x[2], y[1]}
+array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(array)
+  return array.sort.reverse
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+  
+end
