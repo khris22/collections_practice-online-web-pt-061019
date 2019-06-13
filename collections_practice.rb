be4540a020489f041bbaec5d12c7f232bbb3e86e
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-13 11:41:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,6 +18,7 @@ def swap_elements(array)
 # array.swap!(array[1],array[2])
 # array.sort_by{|x,y| x[1], y[2] = x[2], y[1]}
   array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
