5411017fa6c883f2bbc167e84116c9a6244ba7b9
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-13 13:14:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,7 +45,7 @@ end
 
 def add_s(array)
   arrar_s = []
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect do |element, index|
    if index != 1
       array_s.push(element + "s")
    else
