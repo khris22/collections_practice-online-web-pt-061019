7f816094a5d6c9e79c7f1f5ffeec3e73e184edec
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-13 12:24:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,14 +35,13 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array.select do |string|
+  array.find do |string|
     string.start_with?("a")
 end
 
 def sum_array(array)
   sum = 0
   array.inject{|sum, n| sum + n}
-  
   end
 end
 
